883c72371e7bc1b1c6481d34ec85c3944c0e83fd
Andrés Cordero <andresmcorderor@gmail.com>
2024-10-22 23:12:27 -0600

    add: modal animations

--- src/components/common/ModalContact.tsx
+++ src/components/common/ModalContact.tsx
@@ -4,11 +4,24 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { ReactNode, useEffect } from 'react';
 
+import { motion, AnimatePresence } from 'framer-motion';
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
 }
+
+const backdropVariants = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+
+const modalVariants = {
+  hidden: { opacity: 0, y: '-50%', scale: 0.8 },
+  visible: { opacity: 1, y: '0%', scale: 1 },
+  exit: { opacity: 0, y: '50%', scale: 0.8 },
+};
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
   useEffect(() => {
@@ -23,25 +36,37 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
     };
   }, [isOpen]);
 
-  if (!isOpen) return null;
-
   return (
-    <div
-      className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md"
-      onClick={onClose}
-    >
-      <div
-        className="relative w-full max-w-md rounded-lg bg-[var(--white)] p-6 shadow-lg"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <button
-          className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md"
           onClick={onClose}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={backdropVariants}
+          transition={{ duration: 0.3 }}
         >
-          X
-        </button>
-        {children}
-      </div>
-    </div>
+          <motion.div
+            className="relative w-full max-w-md rounded-lg bg-[var(--white)] p-6 shadow-lg"
+            onClick={(e) => e.stopPropagation()}
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ type: 'spring', stiffness: 300, damping: 25 }}
+          >
+            <button
+              className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
+              onClick={onClose}
+            >
+              X
+            </button>
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
